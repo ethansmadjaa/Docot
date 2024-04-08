fb8c899d7b76d421df4aab470206e98408956e84
Ethan Smadja <ethansmadja@icloud.com>
2024-04-08 22:00:38 +0200

    :lipstick: Update MainMenu layout and add title label

--- src/View/MainMenu.java
+++ src/View/MainMenu.java
@@ -1,16 +1,29 @@
 package View;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 
 public class MainMenu {
-    private JFrame frame;
 
 
     public MainMenu(JFrame frame) {
-        this.frame = frame;
 
         frame.getContentPane().removeAll();
+
+        frame.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        // Create and add title
+        JLabel title = new JLabel("Doctolib");
+        title.setFont(new Font("Century Gothic", Font.BOLD, 48));
+        title.setForeground(Color.BLUE);
+        c.gridx = 0;
+        c.gridy = 0;
+        c.gridwidth = GridBagConstraints.REMAINDER;
+        c.insets = new Insets(20, 0, 20, 0);
+        c.anchor = GridBagConstraints.CENTER;
+        frame.add(title, c);
 
         JButton button1 = new JButton("Button 1");
         button1.setBounds(50, 100, 117, 29);
@@ -22,7 +35,7 @@ public class MainMenu {
         frame.getContentPane().add(button1);
 
         JButton button2 = new JButton("Button 2");
-        button2.setBounds(200, 100, 117, 29);
+        button2.setBounds(200, 100, 120, 30);
         button2.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 function2();
