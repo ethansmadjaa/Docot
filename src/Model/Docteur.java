c28f8b7922c32cee6b2ecf0712141980fb048942
Ethan Smadja <ethansmadja@icloud.com>
2024-04-09 12:43:43 +0200

    :sparkles: Added new Docteur model class

---
+++ src/Model/Docteur.java
@@ -0,0 +1,19 @@
+package Model;
+
+public class Docteur {
+
+    private String nom;
+
+    private String prenom;
+
+    private String lieu;
+
+    private String specialité;
+
+    public Docteur(String nom, String prenom, String lieu, String specialité) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.lieu = lieu;
+        this.specialité = specialité;
+    }
+}
