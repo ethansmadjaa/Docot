4cc929bcaaf6c06e947374c1948de21764819368
Ethan Smadja <ethansmadja@icloud.com>
2024-04-09 17:23:35 +0200

    :sparkles: Add method to display doctor results in Results view

--- src/View/Results.java
+++ src/View/Results.java
@@ -1,4 +1,25 @@
 package View;
 
+import Model.Docteur;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class Results {
+
+        public static void viewResults(JScrollPane scrollPane, Docteur docteur){
+            JPanel panel = new JPanel(new BorderLayout());
+
+            panel.add(createLabel("Nom: ", docteur.getNom()), BorderLayout.NORTH);
+            panel.add(createLabel("Prénom: ", docteur.getPrenom()), BorderLayout.WEST);
+            panel.add(createLabel("Lieu: ", docteur.getLieu()), BorderLayout.EAST);
+            panel.add(createLabel("Specialité: ", docteur.getSpecialite()), BorderLayout.CENTER);
+
+            scrollPane.setViewportView(panel);
+        }
+
+        private static JLabel createLabel(String prefix, String text) {
+            return new JLabel(prefix + text);
+        }
 }
+
