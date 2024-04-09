6d89616f98ecc51a1072b2ed9c32350a32228d11
Ethan Smadja <ethansmadja@icloud.com>
2024-04-09 12:44:02 +0200

    🔍 Added Search model with doctor search functionality

---
+++ src/Model/Search.java
@@ -0,0 +1,86 @@
+package Model;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Search extends DaoImpl {
+    private static final String SQL_SEARCH_DOCTEUR =
+            "SELECT docID FROM docteur WHERE nom LIKE ? OR lieu LIKE ? OR specialite LIKE ?";
+    private static final String SQL_SEARCH_DOCTEUR_BY_ID =
+            "SELECT * FROM docteur WHERE id = ?";
+    public List<Integer> searchResults(String content) throws SQLException, ClassNotFoundException {
+
+        List<Integer> results = new ArrayList<>();
+
+        connect();
+
+        String searchQuery = "%" + content + "%";
+
+        try (PreparedStatement pstmt = conn.prepareStatement(SQL_SEARCH_DOCTEUR)) {
+
+            pstmt.setString(1, searchQuery);
+            pstmt.setString(2, searchQuery);
+            pstmt.setString(3, searchQuery);
+
+            rset = pstmt.executeQuery();
+
+            while (rset.next()) {
+                results.add(rset.getInt("docID"));
+            }
+
+            if (results.isEmpty()) {
+                System.out.println("No results found");
+                results.add(0);
+            }
+            else {
+                System.out.println("Results:");
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Database access error:");
+            e.printStackTrace();
+            throw e;
+        }
+        finally {
+            disconnect();
+        }
+
+        return results;
+    }
+
+    public ArrayList<Docteur> searchDocteur(List<Integer> idList) throws SQLException, ClassNotFoundException {
+
+        connect();
+
+        ArrayList<Docteur> docteurs = new ArrayList<>();
+
+        for (Integer id : idList) {
+            Docteur docteur = null;
+            try (PreparedStatement pstmt = conn.prepareStatement(SQL_SEARCH_DOCTEUR_BY_ID)) {
+                pstmt.setInt(1, id);
+                rset = pstmt.executeQuery();
+                if (rset.next()) {
+                    docteur = new Docteur(
+                            rset.getString("nom"),
+                            rset.getString("prenom"),
+                            rset.getString("specialite"),
+                            rset.getString("Lieu"));
+                }
+
+            } catch (SQLException e) {
+                System.out.println("Database access error:");
+                e.printStackTrace();
+                throw e;
+            }
+            if (docteur != null) {
+                docteurs.add(docteur);
+            }
+        }
+        disconnect();
+        return docteurs;
+    }
+}
+
