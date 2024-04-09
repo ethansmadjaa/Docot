4b180976bb695feb3e0d582def397a672f08efed
Ethan Smadja <ethansmadja@icloud.com>
2024-04-09 17:23:21 +0200

    🔒 Make attributes in Docteur class final and add getters

--- src/Model/Docteur.java
+++ src/Model/Docteur.java
@@ -2,18 +2,38 @@ package Model;
 
 public class Docteur {
 
-    private String nom;
+    private final String nom;
 
-    private String prenom;
+    private final String prenom;
 
-    private String lieu;
+    private final String lieu;
 
-    private String specialité;
+    private final String specialite;
 
-    public Docteur(String nom, String prenom, String lieu, String specialité) {
+    public Docteur(String nom, String prenom, String lieu, String specialite) {
         this.nom = nom;
         this.prenom = prenom;
         this.lieu = lieu;
-        this.specialité = specialité;
+        this.specialite = specialite;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getLieu() {
+        return lieu;
+    }
+
+    public String getSpecialite(){
+        return specialite;
+    }
+
+    public String toString(){
+        return nom + " " + prenom + " " + lieu + " " + specialite;
     }
 }
