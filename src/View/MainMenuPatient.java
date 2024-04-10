2e84ed2bef58c67805bef4d78ce2a6fee6ecc9f4
Ethan Smadja <ethansmadja@icloud.com>
2024-04-10 12:10:58 +0200

    🚚 Rename MainMenu to MainMenuPatient and adjust methods for patient input

--- src/View/MainMenuPatient.java
+++ src/View/MainMenuPatient.java
@@ -1,21 +1,30 @@
 package View;
 import Controller.SearchController;
 import Model.Docteur;
+import Model.Patient;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.sql.SQLException;
 import java.util.ArrayList;
-public class MainMenu {
+public class MainMenuPatient {
+
     private JTextField searchBar = new JTextField(20);
     private JTextArea resultsArea = new JTextArea(5, 20);
-    public MainMenu(JFrame frame) {
-        SearchController searchController = new SearchController();
-        frame.getContentPane().removeAll();
-        frame.setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-        c.fill = GridBagConstraints.HORIZONTAL;
 
+    public MainMenuPatient(JFrame frame, Patient patient) {
+        Patient p = patient;
+
+        SearchController searchController = new SearchController();
+
+        frame.getContentPane().removeAll();
+
+        frame.setLayout(new GridBagLayout());
+
+        GridBagConstraints c = new GridBagConstraints();
+
+        c.fill = GridBagConstraints.HORIZONTAL;
 
         // Title
         JLabel title = new JLabel("Doctolib", SwingConstants.CENTER);
@@ -28,11 +37,13 @@ public class MainMenu {
         c.gridwidth = GridBagConstraints.REMAINDER;
         c.insets = new Insets(20, 0, 20, 0);
         frame.add(title, c);
+
         // Search label
         JLabel label = new JLabel("Qu'est ce qu'il ne va pas aujourd'hui ?", SwingConstants.CENTER);
         label.setFont(new Font("Century Gothic", Font.BOLD, 24));
         c.gridy = 1;
         frame.add(label, c);
+
         // Search bar
         searchBar.setFont(new Font("Century Gothic", Font.PLAIN, 18));
         c.gridy = 2;
@@ -44,7 +55,6 @@ public class MainMenu {
         c.gridy = 3;
         c.insets = new Insets(10, 0, 10, 0);
         frame.add(searchButton, c);
-
 
         // Results area
         resultsArea.setEditable(false);
@@ -65,7 +75,7 @@ public class MainMenu {
             ArrayList<Docteur> docteurs = searchController.getSearchResults(searchBar.getText());
             resultsArea.setText("");
             for (Docteur docteur : docteurs) {
-                Results.viewResults(scrollPane, docteur);
+                ViewResults.viewDoctors(scrollPane, docteur);
             }
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
