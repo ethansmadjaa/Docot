ffd7db1c54a4ba451a61535f22784b366df4081e
Ethan Smadja <ethansmadja@icloud.com>
2024-04-10 12:11:51 +0200

    🔨 Refactored viewResults to viewDoctors and added email field

--- src/View/ViewResults.java
+++ src/View/ViewResults.java
@@ -7,15 +7,20 @@ import java.awt.*;
 
 public class ViewResults {
 
-        public static void viewResults(JScrollPane scrollPane, Docteur docteur){
+        public static void viewDoctors(JScrollPane scrollPane, Docteur docteur){
             JPanel panel = new JPanel(new BorderLayout());
 
             panel.add(createLabel("Nom: ", docteur.getNom()), BorderLayout.NORTH);
             panel.add(createLabel("Prénom: ", docteur.getPrenom()), BorderLayout.WEST);
             panel.add(createLabel("Lieu: ", docteur.getLieu()), BorderLayout.EAST);
             panel.add(createLabel("Specialité: ", docteur.getSpecialite()), BorderLayout.CENTER);
+            panel.add(createLabel("Email: ", docteur.getEmail()), BorderLayout.SOUTH);
 
             scrollPane.setViewportView(panel);
+        }
+
+        public static void searchRdv(JFrame frame, int id){
+
         }
 
         private static JLabel createLabel(String prefix, String text) {
