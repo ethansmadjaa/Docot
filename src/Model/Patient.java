f20ae7c47777cc351206646b2c95ae87af15ea90
Ethan Smadja <ethansmadja@icloud.com>
2024-04-10 12:11:05 +0200

    🆕 Create Patient model with DB integration

---
+++ src/Model/Patient.java
@@ -0,0 +1,40 @@
+package Model;
+
+import java.sql.*;
+
+public class Patient extends DaoImpl {
+
+    private int id;
+    private String Nom;
+    private String Prenom;
+    private String Email;
+
+    public Patient(String email, String password) throws SQLException, ClassNotFoundException {
+        this.Email = email;
+        fetchPatientInfoFromDB(email, password);
+    }
+
+    private void fetchPatientInfoFromDB(String email, String password) throws SQLException, ClassNotFoundException {
+        String patientInfo = "";
+        String sql = "SELECT PatientID, Nom, Prenom FROM Patients WHERE Email = ? AND Motdepasse = ?";
+
+        connect();
+
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, email);
+            stmt.setString(2, password);
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    this.id = rs.getInt("PatientID");
+                    this.Nom = rs.getString("Nom");
+                    this.Prenom = rs.getString("Prenom");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
