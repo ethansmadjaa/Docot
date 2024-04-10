87081178d0246adf6c1a117c961751102d5d00f9
Ethan Smadja <ethansmadja@icloud.com>
2024-04-10 12:10:11 +0200

    :card_file_box: Converted Doctor model to DAO implementation

--- src/Model/Docteur.java
+++ src/Model/Docteur.java
@@ -1,23 +1,34 @@
 package Model;
 
-public class Docteur {
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-    private final String nom;
+public class Docteur extends DaoImpl{
 
-    private final String prenom;
+    private  int id;
 
-    private final String lieu;
+    private  String nom;
 
-    private final String specialite;
+    private  String prenom;
+
+    private  String lieu;
+
+    private  String specialite;
 
     private final String email;
 
-    public Docteur(String nom, String prenom,String specialite,  String email, String lieu) {
+    public Docteur(int id, String nom, String prenom,String specialite,  String email, String lieu) {
+        this.id = id;
         this.nom = nom;
         this.prenom = prenom;
         this.specialite = specialite;
         this.email = email;
         this.lieu = lieu;
+    }
+    public Docteur(String email, String password) throws SQLException, ClassNotFoundException {
+        this.email=email;
+        fetchDocteurFromDatabase(email, password);
     }
 
     public String getNom() {
@@ -40,7 +51,35 @@ public class Docteur {
         return email;
     }
 
+    public int getId() {
+        return id;
+    }
+
     public String toString(){
         return nom + " " + prenom + " " + lieu + " " + specialite;
+    }
+
+    private void fetchDocteurFromDatabase(String email, String password) throws SQLException, ClassNotFoundException {
+        String sql = "SELECT MedecinID, Nom, Prenom, Specialite, Lieu FROM Medecins WHERE Email = ? AND Motdepasse = ?";
+
+        connect();
+
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, email);
+            stmt.setString(2, password);
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    this.id = rs.getInt("MedecinID");
+                    this.nom = rs.getString("Nom");
+                    this.prenom = rs.getString("Prenom");
+                    this.specialite = rs.getString("Specialite");
+                    this.lieu = rs.getString("Lieu");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
