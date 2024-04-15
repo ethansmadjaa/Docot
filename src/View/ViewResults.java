c460d62adaa825d28d19cd9c481bc9bbf0f4a63a
Ethan Smadja <ethansmadja@icloud.com>
2024-04-15 16:15:26 +0200

    :twisted_rightwards_arrows: Refactored ViewResults to support array of doctors and added card layout

--- src/View/ViewResults.java
+++ src/View/ViewResults.java
@@ -1,30 +1,48 @@
 package View;
 
-import Model.Docteur;
-
 import javax.swing.*;
 import java.awt.*;
+import Model.Docteur;
+import Model.RdvModel;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ViewResults {
 
-        public static void viewDoctors(JScrollPane scrollPane, Docteur docteur){
-            JPanel panel = new JPanel(new BorderLayout());
+    private static int doctorIndex = 0;
 
-            panel.add(createLabel("Nom: ", docteur.getNom()), BorderLayout.NORTH);
-            panel.add(createLabel("Prénom: ", docteur.getPrenom()), BorderLayout.WEST);
-            panel.add(createLabel("Lieu: ", docteur.getLieu()), BorderLayout.EAST);
-            panel.add(createLabel("Specialité: ", docteur.getSpecialite()), BorderLayout.CENTER);
-            panel.add(createLabel("Email: ", docteur.getEmail()), BorderLayout.SOUTH);
+    public static void viewDoctors(JScrollPane scrollPane, ArrayList<Docteur> doctors){
+        JPanel cards = new JPanel(new CardLayout());
 
-            scrollPane.setViewportView(panel);
+        for (Docteur doctor : doctors) {
+            JPanel card = new JPanel(new BorderLayout());
+            card.add(createLabel("Nom: ", doctor.getNom()), BorderLayout.NORTH);
+            card.add(createLabel("Prénom: ", doctor.getPrenom()), BorderLayout.WEST);
+            card.add(createLabel("Lieu: ", doctor.getLieu()), BorderLayout.CENTER);
+            card.add(createLabel("Specialité: ", doctor.getSpecialite()), BorderLayout.EAST);
+            card.add(createLabel("Email: ", doctor.getEmail()), BorderLayout.SOUTH);
+
+            cards.add(card, "card" + doctorIndex);
+            doctorIndex++;
         }
 
-        public static void searchRdv(JFrame frame, int id){
+        scrollPane.setViewportView(cards);
+    }
 
-        }
+    public static void showCard(JScrollPane scrollPane, int index){
+        JPanel cards = (JPanel) scrollPane.getViewport().getView();
+        CardLayout cl = (CardLayout)(cards.getLayout());
+        cl.show(cards, "card" + index);
+    }
 
-        private static JLabel createLabel(String prefix, String text) {
-            return new JLabel(prefix + text);
-        }
+    private static JLabel createLabel(String prefix, String text) {
+        return new JLabel(prefix + text);
+    }
+
+    public static void searchRdv(JFrame frame, int id) throws SQLException, ClassNotFoundException {
+        RdvModel rdvModel = new RdvModel();
+        rdvModel.searchRendezVous(id);
+    }
 }
-
