a464ee7a6fbe134de52f377db61548e9ac037049
Ethan Smadja <ethansmadja@icloud.com>
2024-04-16 12:18:36 +0200

    :art: Revamp login frame to enhance UI

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,24 @@
-import view.LoginFrame;
+import view.Login;
+
+import javax.swing.*;
+
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class Main {
 
     public static void main(String[] args){
-        LoginFrame loginFrame = new LoginFrame();
+        JFrame frame = new JFrame();
+        System.out.println("Creating Login Frame");
+        frame.setTitle("Login");
+        frame.setSize(1200, 800);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        // Create and set the LoginPanel as the content panel
+        Login login = new Login(frame);
+        frame.setContentPane(login);
+
+        frame.setVisible(true);
+
     }
 }
