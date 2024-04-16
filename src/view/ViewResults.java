c6d10c831f0dcc7c91cc35d847bd9d91bc7f7e57
Ethan Smadja <ethansmadja@icloud.com>
2024-04-16 12:45:18 +0200

    :sparkles: Add booking functionality in doctors view results

--- src/view/ViewResults.java
+++ src/view/ViewResults.java
@@ -2,40 +2,56 @@ package view;
 
 import javax.swing.*;
 import java.awt.*;
+
+import controller.RdvController;
 import model.Docteur;
+import model.Patient;
 import model.RdvModel;
 import model.RendezVous;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class ViewResults {
 
-    public static void viewDoctors(JScrollPane scrollPane, ArrayList<Docteur> doctors){
+    public static void viewDoctors(JScrollPane scrollPane, ArrayList<Docteur> docteurs, Patient patient){
         JPanel cards = new JPanel();
         cards.setLayout(new BoxLayout(cards, BoxLayout.Y_AXIS));
         cards.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
-        for (Docteur doctor : doctors) {
+        for (Docteur docteur : docteurs) {
 
             JPanel card = new JPanel();
             card.setLayout(new GridBagLayout());
             GridBagConstraints gbc = new GridBagConstraints();
+            JButton button = new JButton("Reserver un Rendez-vous avec le Dr. " + docteur.getNom());
+
+            RdvController rdvController = new RdvController(docteur, patient);
+
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    rdvController.reserver();
+                }
+            });
 
             gbc.gridwidth = GridBagConstraints.REMAINDER;
             gbc.weightx = 1;
             gbc.fill = GridBagConstraints.HORIZONTAL;
             gbc.insets = new Insets(5, 5, 5, 5);
 
-            card.add(createLabel("Nom: ", doctor.getNom()), gbc);
-            card.add(createLabel("Prénom: ", doctor.getPrenom()), gbc);
-            card.add(createLabel("Lieu: ", doctor.getLieu()), gbc);
-            card.add(createLabel("Spécialité: ", doctor.getSpecialite()), gbc);
-            card.add(createLabel("Email: ", doctor.getEmail()), gbc);
+            card.add(createLabel("Nom: ", docteur.getNom()), gbc);
+            card.add(createLabel("Prénom: ", docteur.getPrenom()), gbc);
+            card.add(createLabel("Lieu: ", docteur.getLieu()), gbc);
+            card.add(createLabel("Spécialité: ", docteur.getSpecialite()), gbc);
+            card.add(createLabel("Email: ", docteur.getEmail()), gbc);
+            card.add(button, gbc);
 
             card.setBorder(BorderFactory.createCompoundBorder(
                     BorderFactory.createEmptyBorder(10, 10, 10, 10),
-                    BorderFactory.createLineBorder(Color.BLACK)
+                    BorderFactory.createLineBorder(Color.BLUE)
             ));
 
             JPanel wrapper = new JPanel(new BorderLayout());
@@ -56,7 +72,7 @@ public class ViewResults {
 
     public static void searchRdv(JScrollPane scrollPane, Docteur docteur) throws SQLException, ClassNotFoundException {
         RdvModel rdvModel = new RdvModel();
-        ArrayList<RendezVous> listRdv = rdvModel.searchRendezVous(docteur.getId());
+        ArrayList<RendezVous> listRdv = rdvModel.searchRendezVousDocID(docteur.getId());
 
         JPanel cards = new JPanel();
         cards.setLayout(new BoxLayout(cards, BoxLayout.Y_AXIS));
