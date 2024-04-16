9b4427d26abf1327a1fcc64ff7e99870757dcc58
Ethan Smadja <ethansmadja@icloud.com>
2024-04-16 12:44:32 +0200

    🆕 Add RdvController with reservation functionality

---
+++ src/controller/RdvController.java
@@ -0,0 +1,35 @@
+package controller;
+
+import model.Docteur;
+import model.Patient;
+import model.RdvModel;
+import model.RendezVous;
+import view.ReserverRdvView;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+
+
+public class RdvController {
+    private final Docteur docteur;
+    private final Patient patient;
+
+    public RdvController(Docteur docteur, Patient patient) {
+        this.docteur = docteur;
+        this.patient = patient;
+    }
+
+    public void reserver(){
+        ReserverRdvView rdvView = new ReserverRdvView(docteur, patient);
+    }
+
+    public boolean checkTimeSlot(Date date) throws SQLException, ClassNotFoundException {
+        RdvModel rdvModel = new RdvModel();
+        ArrayList<RendezVous> rdvList = rdvModel.searchRdvdocIdPatId(docteur.getId(), patient.getId());
+        for(RendezVous rdv : rdvList){
+            return !rdv.getDateEtHeure().equals(date);
+        }
+        return false;
+    }
+}
