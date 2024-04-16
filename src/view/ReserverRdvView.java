1ee04a33277c114ef982cc27bd48ce53d1802f41
Ethan Smadja <ethansmadja@icloud.com>
2024-04-16 15:30:22 +0200

    :sparkles: Extended appointment scheduling functionality in ReserverRdvView.java

--- src/view/ReserverRdvView.java
+++ src/view/ReserverRdvView.java
@@ -4,80 +4,148 @@ import com.toedter.calendar.JCalendar;
 import controller.RdvController;
 import model.Docteur;
 import model.Patient;
+import model.RendezVous;
 
 import javax.swing.*;
 import java.awt.*;
 import java.sql.Date;
 import java.sql.SQLException;
 import java.time.*;
-import java.time.format.DateTimeFormatter;
-
-import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+import java.util.ArrayList;
 
 public class ReserverRdvView {
 
     private final Docteur docteur;
     private final Patient patient;
     private final RdvController rdvController;
+    private final JDialog reserverDialog;
 
-    public ReserverRdvView(Docteur docteur, Patient patient){
+    public ReserverRdvView(Docteur docteur, Patient patient) {
         this.docteur = docteur;
         this.patient = patient;
 
         rdvController = new RdvController(docteur, patient);
 
-        JDialog reserverFrame = new JDialog();
-        reserverFrame.setTitle("Reserver un rdv avec Dr." + docteur.getNom());
-        reserverFrame.setSize(800, 800);
-        reserverFrame.setLocationRelativeTo(null);
-        reserverFrame.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-        reserverFrame.setLayout(new BorderLayout());
+        reserverDialog = new JDialog();
+        reserverDialog.setTitle("Reserver un rdv avec Dr." + docteur.getNom());
+        reserverDialog.setSize(800, 800);
+        reserverDialog.setLocationRelativeTo(null);
+        reserverDialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        reserverDialog.setLayout(new BorderLayout());
 
         JCalendar calendar = new JCalendar(); // JCalendar to display a calendar
-        JPanel timeSlotsPanel = new JPanel(new GridLayout(0, 2)); // One appointment per row
+        JPanel timeSlotsPanel = new JPanel(new GridLayout(0, 2)); // Two appointments per row
 
-        reserverFrame.add(calendar, BorderLayout.NORTH); // Calendar at the top
-        reserverFrame.add(new JScrollPane(timeSlotsPanel), BorderLayout.CENTER); // Scrollable timeSlotsPanel at the center
+        reserverDialog.add(calendar, BorderLayout.NORTH); // Calendar at the top
+        reserverDialog.add(new JScrollPane(timeSlotsPanel), BorderLayout.CENTER); // Scrollable timeSlotsPanel at the center
 
         // Add a property change listener to the calendar
         calendar.addPropertyChangeListener("calendar", event -> {
-            java.util.Date selectedDate = calendar.getDate();
-            try {
-                updateTimeSlots(selectedDate, timeSlotsPanel); // Method to update the time slots
-            } catch (SQLException | ClassNotFoundException e) {
-                throw new RuntimeException(e);
+            java.util.Date utilDate = calendar.getDate();
+            java.sql.Date selectedDate = new java.sql.Date(utilDate.getTime());
+            LocalDateTime now = LocalDateTime.now();
+            if (selectedDate.before(java.sql.Timestamp.valueOf(now))) {
+                JOptionPane.showMessageDialog(reserverDialog,
+                        "La date sélectionnée est antérieure " +
+                        "à la date d'aujourd'hui," +
+                        " veuillez choisir une autre date.", "Date invalide", JOptionPane.ERROR_MESSAGE);
+            } else {
+                try {
+                    updateTimeSlots(selectedDate, timeSlotsPanel); // Method to update the time slots
+                } catch (SQLException | ClassNotFoundException e) {
+                    e.printStackTrace(); // Consider displaying an error dialog instead
+                }
             }
         });
 
-        reserverFrame.setVisible(true);
+        reserverDialog.setVisible(true);
     }
 
-    private void updateTimeSlots(java.util.Date selectedDate, JPanel timeSlotsPanel) throws SQLException, ClassNotFoundException {
-        JLabel Title = new JLabel(String.format(
-                "Rendez-vous disponibles le " + selectedDate ));
+    private JLabel createTitleLabel(Date selectedDate) {
+        JLabel title = new JLabel(String.format("Rendez-vous disponibles le %1$td %1$tB %1$tY", selectedDate));
+        title.setHorizontalAlignment(SwingConstants.CENTER);
+        title.setFont(new Font("Century Gothic", Font.BOLD, 18));
+        title.setForeground(Color.BLUE);
+        return title;
+    }
 
+    private JSeparator createSeparator() {
+        return new JSeparator();
+    }
+
+    private void updateTimeSlots(Date selectedDate, JPanel timeSlotsPanel) throws SQLException, ClassNotFoundException {
+        // Clear the timeSlotsPanel and set up a title label
         timeSlotsPanel.removeAll();
+        JLabel title = createTitleLabel(selectedDate);
+        timeSlotsPanel.add(title);
 
-        LocalTime startTime = LocalTime.of(9, 0); // Début de la journée
-        LocalTime endTime = LocalTime.of(18, 0); // Fin de la journée
+        JSeparator separator = createSeparator();
+        timeSlotsPanel.add(separator);
 
-        do{
-            if (!isTimeSlotTaken(selectedDate)) {
-                JButton timeSlotButton = new JButton(startTime.format(DateTimeFormatter.ofPattern("HH:mm")));
-                timeSlotButton.addActionListener(e -> {
-                    // Handle reservation logic here
-                });
-                timeSlotsPanel.add(timeSlotButton);
-            }
-            startTime = startTime.plusMinutes(30); // Next time slot
-        }while (startTime.isBefore(endTime));
+        // Creat time slot buttons
+        createTimeSlotButtons(selectedDate, timeSlotsPanel);
 
-            // Refresh and repaint the panel to display buttons
+        // Refresh and repaint the panel to display buttons
         timeSlotsPanel.revalidate();
         timeSlotsPanel.repaint();
     }
 
-        private boolean isTimeSlotTaken(java.util.Date dateTime) throws SQLException, ClassNotFoundException {
-        return  rdvController.checkTimeSlot(dateTime);
+    private void createTimeSlotButtons(Date selectedDate, JPanel timeSlotsPanel) throws SQLException, ClassNotFoundException {
+        LocalTime startTime = LocalTime.of(9, 0); // Start of working hours
+        LocalTime endTime = LocalTime.of(18, 0); // End of working hours
+        ArrayList<RendezVous> rdv = getRendezVousByDocteur();
+        do{
+             if (!isTimeSlotTaken(rdv, startTime, selectedDate.toLocalDate())) {
+                JButton timeSlotButton = new JButton(String.valueOf(startTime));
+                 LocalTime finalStartTime = startTime;
+                 timeSlotButton.addActionListener(e -> {
+                     try {
+                         confirmReservation(selectedDate, finalStartTime);
+                     } catch (SQLException | ClassNotFoundException ex) {
+                         throw new RuntimeException(ex);
+                     }
+                 });
+                timeSlotsPanel.add(timeSlotButton);
+            }
+            startTime = startTime.plusMinutes(30); // Increment to the next time slot
+        }while (startTime.isBefore(endTime));
+    }
+
+    private void confirmReservation(Date selectedDate, LocalTime time) throws SQLException, ClassNotFoundException {
+        int option = JOptionPane.showConfirmDialog(
+                reserverDialog,
+                "Confirmer la réservation\nPour le "
+                        + selectedDate + " à " + time
+                        + " avec le docteur " + docteur.getNom() + " ?",
+                "Confirmation du RDV",
+                JOptionPane.YES_NO_OPTION);
+
+        if(JOptionPane.YES_OPTION == option){
+            if(rdvController.reserverRdv(selectedDate, time, docteur.getId(), patient.getId())){
+                JOptionPane.showMessageDialog(
+                        reserverDialog,
+                        "Réservation réussie !",
+                        "Confirmation",
+                        JOptionPane.INFORMATION_MESSAGE);
+
+                reserverDialog.dispose();
+
+            };
+        };
+    }
+
+    private ArrayList<RendezVous> getRendezVousByDocteur() throws SQLException, ClassNotFoundException {
+        return rdvController.getDocteurRdv();
+    }
+
+    private boolean isTimeSlotTaken(ArrayList<RendezVous> appointments, LocalTime timeSlot, LocalDate date) {
+        for (RendezVous appointment : appointments) {
+            System.out.println(appointment.getDate()+ " " + appointment.getHeure() + " " + timeSlot);
+            if (appointment.getDate().equals(date) && appointment.getHeure().equals(timeSlot)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
+
