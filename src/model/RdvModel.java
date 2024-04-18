574cc96eb1dfdd78b1ff068ceaef926b48be1e9c
Ethan Smadja <ethansmadja@icloud.com>
2024-04-18 18:54:18 +0200

    🔥 Remove commented SQL queries from RdvModel

--- src/model/RdvModel.java
+++ src/model/RdvModel.java
@@ -150,28 +150,3 @@ public class RdvModel extends DaoImpl{
         }
     }
 }
-/**
- *
- * Utiliser un jseparator
-        Mettre à jour le status du rdv en passé
-
- UPDATE RendezVous SET Status = 'Passé' WHERE DateEtHeure < NOW();
-
-                Inserer un nouveau RDV
-
- INSERT INTO RendezVous (PatientID, MedecinID, DateEtHeure, Raison) VALUES (?, ?, ?, ?);
-
-                Voir les rdv pris par medecin ID
-
- SELECT * FROM RendezVous
- JOIN Patients ON RendezVous.PatientID = Patients.PatientID
- JOIN Medecins ON RendezVous.MedecinID = Medecins.MedecinID
- WHERE RendezVous.MedecinID = ? AND RendezVous.DateEtHeure > NOW()
- ORDER BY RendezVous.DateEtHeure;
-
- pour Voir les rdv dispo, faire cette requete et afficher les horraires qui ne sont pas dans ce resultat
-
- SELECT DateEtHeure FROM RendezVous WHERE MedecinID = ? AND DateEtHeure > NOW() ORDER BY DateEtHeure;
-
- */
-
