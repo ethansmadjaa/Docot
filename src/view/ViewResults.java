fe2b68b2c4968fb387d2273f30bc90bf42fa41b7
Ethan Smadja <ethansmadja@icloud.com>
2024-04-18 16:41:07 +0200

    :sparkles: Added functionality to cancel appointments for both doctors and patients

--- src/view/ViewResults.java
+++ src/view/ViewResults.java
@@ -70,7 +70,7 @@ public class ViewResults {
         return new JLabel(prefix + text);
     }
 
-    public static void searchRdv(JScrollPane scrollPane, Docteur docteur) throws SQLException, ClassNotFoundException {
+    public static void searchRdvDoc(JScrollPane scrollPane, Docteur docteur) throws SQLException, ClassNotFoundException {
         RdvModel rdvModel = new RdvModel();
         ArrayList<RendezVous> listRdv = rdvModel.searchRendezVousDocID(docteur.getId());
 
@@ -100,6 +100,37 @@ public class ViewResults {
                     BorderFactory.createLineBorder(Color.BLACK)
             ));
 
+            Patient patient = new Patient(rdv.getPatientId());
+
+            JButton cancelButton = new JButton("Annuler le Rendez-Vous avec le Mr. " + patient.getNom());
+
+            cancelButton.addActionListener(e -> {
+                int userChoice = JOptionPane.showConfirmDialog(
+                        cards,
+                        "Confirmer l'annulation du RDV avec Mr. "
+                                + patient.getNom()
+                                + "\n pour le "
+                                + rdv.getDate() + ".",
+                        "Annulation d'un RDV",
+                        JOptionPane.YES_NO_OPTION);
+
+                if (userChoice == JOptionPane.YES_OPTION) {
+                    try {
+                        if (RdvController.cancelRdv(rdv.getRendezVousID())) {
+                            JOptionPane.showMessageDialog(
+                                    cards,
+                                    "Rendez vous annulé avec Succès !",
+                                    "Confirmation",
+                                    JOptionPane.INFORMATION_MESSAGE);
+                        }
+                    } catch (SQLException | ClassNotFoundException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+            });
+
+            card.add(cancelButton, gbc);
+
             JPanel wrapper = new JPanel(new BorderLayout());
             wrapper.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
             wrapper.add(card);
@@ -108,5 +139,79 @@ public class ViewResults {
 
         scrollPane.setViewportView(cards);
         scrollPane.getViewport().setBackground(Color.WHITE);
+    }
+
+    public static void searchRdvPat(JScrollPane scrollPane, Patient patient) throws SQLException, ClassNotFoundException {
+        RdvModel rdvModel = new RdvModel();
+        ArrayList<RendezVous> listRdv = rdvModel.searchRendezVousPatID(patient.getId());
+
+        JPanel cards = new JPanel();
+        cards.setLayout(new BoxLayout(cards, BoxLayout.Y_AXIS));
+        cards.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        for (RendezVous rdv : listRdv) {
+
+            JPanel card = new JPanel();
+            card.setLayout(new GridBagLayout());
+            GridBagConstraints gbc = new GridBagConstraints();
+
+            gbc.gridwidth = GridBagConstraints.REMAINDER;
+            gbc.weightx = 1;
+            gbc.fill = GridBagConstraints.HORIZONTAL;
+            gbc.insets = new Insets(5, 5, 5, 5);
+
+            card.add(createLabel("Date: ", rdv.getDate().toString()), gbc);
+            card.add(createLabel("Heure: ", rdv.getHeure().toString()), gbc);
+            card.add(createLabel("Status: ", rdv.getStatus()), gbc);
+            card.add(createLabel("Infos du medecin:\n ", rdv.getDoctorInfo()), gbc);
+            card.add(createLabel("Lieu: ", rdv.getLieu()), gbc);
+
+            card.setBorder(BorderFactory.createCompoundBorder(
+                    BorderFactory.createEmptyBorder(10, 10, 10, 10),
+                    BorderFactory.createLineBorder(Color.BLACK)
+            ));
+
+            Docteur docteur = new Docteur(rdv.getDocId());
+
+            JButton cancelButton = new JButton("Annuler le Rendez-Vous avec le Dr. " + docteur.getNom());
+
+            cancelButton.addActionListener(e -> {
+                int userChoice = JOptionPane.showConfirmDialog(
+                        cards,
+                        "Confirmer l'annulation du RDV avec le Dr. "
+                                + docteur.getNom()
+                                + "\n pour le "
+                                + rdv.getDate() + ".",
+                        "Annulation d'un RDV",
+                        JOptionPane.YES_NO_OPTION);
+
+                if (userChoice == JOptionPane.YES_OPTION) {
+                    try {
+                        if (RdvController.cancelRdv(rdv.getRendezVousID())) {
+                            JOptionPane.showMessageDialog(
+                                    cards,
+                                    "Rendez vous annulé avec Succès !",
+                                    "Confirmation",
+                                    JOptionPane.INFORMATION_MESSAGE);
+
+                        }
+                    } catch (SQLException | ClassNotFoundException ex) {
+                        throw new RuntimeException(ex);
+
+                    }
+                }
+
+            });
+
+            card.add(cancelButton, gbc);
+            JPanel wrapper = new JPanel(new BorderLayout());
+            wrapper.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+            wrapper.add(card);
+            cards.add(wrapper);
+        }
+
+        scrollPane.setViewportView(cards);
+        scrollPane.getViewport().setBackground(Color.WHITE);
+
     }
 }
